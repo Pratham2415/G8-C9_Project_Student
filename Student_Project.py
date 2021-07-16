c7a98190b34c1d31d9cdba49d1b344db603277b2
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-07-16 16:48:28 +0530

    Update Student_Project.py

--- Student_Project.py
+++ Student_Project.py
@@ -5,12 +5,12 @@ turtle.speed(10)
 n=1
 colors=['red','green','blue','yellow','purple']
 
-for angle in range(0,360,15):
-    n=n+1
-    if n==5:
-        n=-1
+for angle in range(#Write the range):
+    #Increment the variable n by 1 units
+    #Write the if block to check whether n=5
+        #Set n variable value to -1
     turtle.color(#Pass lists elements one by one)
-    turtle.seth(#Pass the variable angle)
+    turtle.seth(angle)
     turtle.circle(100)
         
 turtle.penup()
